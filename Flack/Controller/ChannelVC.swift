8c2e12ae5cc609aa0f70118afb0e7e5a757abffb
Sohel Dhengre <>
2017-12-16 17:39:56 +0530

    setup rear and front view

---
+++ Flack/Controller/ChannelVC.swift
@@ -0,0 +1,21 @@
+//
+//  ChannelVC.swift
+//  Flack
+//
+//  Created by Sohel Dhengre on 16/12/17.
+//  Copyright © 2017 Sohel Dengre. All rights reserved.
+//
+
+import UIKit
+
+class ChannelVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        self.revealViewController().rearViewRevealWidth = view.frame.size.width - 60
+    }
+
+    
+
+}
---
+++ Flack/Controller/ChatVC.swift
@@ -0,0 +1,27 @@
+//
+//  ChatVC.swift
+//  Flack
+//
+//  Created by Sohel Dhengre on 16/12/17.
+//  Copyright © 2017 Sohel Dengre. All rights reserved.
+//
+
+import UIKit
+
+class ChatVC: UIViewController {
+
+    //outlets
+    @IBOutlet weak var menuBtn: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        menuBtn.addTarget(self.revealViewController(), action: #selector(SWRevealViewController.revealToggle(_:)), for: .touchUpInside)
+        self.view.addGestureRecognizer(self.revealViewController().panGestureRecognizer())
+        self.view.addGestureRecognizer(self.revealViewController().tapGestureRecognizer())
+        
+    }
+
+   
+
+}
